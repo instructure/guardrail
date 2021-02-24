7025f9f0266323a88d466b882394f5e870dcf0ce
Cody Cutrer <cody@instructure.com>
2021-02-24 09:30:41 -0700

    re-write for rails 6.1

--- lib/guard_rail.rb
+++ lib/guard_rail.rb
@@ -1,114 +1,19 @@
-require 'set'
+# frozen_string_literal: true
 
 module GuardRail
   class << self
-    attr_accessor :primary_environment_name
-    GuardRail.primary_environment_name = :primary
-
     def environment
-      Thread.current[:guard_rail_environment] ||= primary_environment_name
+      ActiveRecord::Base.current_role
     end
 
-    def global_config
-      @global_config ||= {}
+    def activate(role)
+      return yield if environment == role
+      ActiveRecord::Base.connected_to(role: role) { yield }
     end
 
-    def activated_environments
-      @activated_environments ||= Set.new()
-    end
-
-    # semi-private
-    def initialize!
-      require 'guard_rail/connection_handler'
-      require 'guard_rail/connection_specification'
-      require 'guard_rail/helper_methods'
-
-      activated_environments << GuardRail.environment
-
-      ActiveRecord::ConnectionAdapters::ConnectionHandler.prepend(ConnectionHandler)
-      ActiveRecord::ConnectionAdapters::ConnectionSpecification.prepend(ConnectionSpecification)
-    end
-
-    def global_config_sequence
-      @global_config_sequence ||= 1
-    end
-
-    def bump_sequence
-      @global_config_sequence ||= 1
-      @global_config_sequence += 1
-      ActiveRecord::Base::connection_handler.clear_all_connections!
-    end
-
-    # for altering other pieces of config (i.e. username)
-    # will force a disconnect
-    def apply_config!(hash)
-      global_config.merge!(hash)
-      bump_sequence
-    end
-
-    def remove_config!(key)
-      global_config.delete(key)
-      bump_sequence
-    end
-
-    def connection_handlers
-      save_handler
-      @connection_handlers
-    end
-
-    # switch environment for the duration of the block
-    # will keep the old connections around
-    def activate(environment)
-      environment ||= primary_environment_name
-      return yield if environment == self.environment
-      begin
-        old_environment = activate!(environment)
-        activated_environments << environment
-        yield
-      ensure
-        Thread.current[:guard_rail_environment] = old_environment
-        ActiveRecord::Base.connection_handler = ensure_handler unless test?
-      end
-    end
-
-    # for use from script/console ONLY
-    def activate!(environment)
-      environment ||= primary_environment_name
-      save_handler
-      old_environment = self.environment
-      Thread.current[:guard_rail_environment] = environment
-      ActiveRecord::Base.connection_handler = ensure_handler unless test?
-      old_environment
-    end
-
-    private
-
-    def test?
-      Rails.env.test?
-    end
-
-    def save_handler
-      @connection_handlers ||= {}
-      @connection_handlers[environment] ||= ActiveRecord::Base.connection_handler
-    end
-
-    def ensure_handler
-      new_handler = @connection_handlers[environment]
-      if !new_handler
-        new_handler = @connection_handlers[environment] = ActiveRecord::ConnectionAdapters::ConnectionHandler.new
-        pools = ActiveRecord::Base.connection_handler.send(:owner_to_pool)
-        pools.each_pair do |model, pool|
-          new_handler.establish_connection(pool.spec.config)
-        end
-      end
-      new_handler
+    def activate!(role)
+      return if environment == role
+      ActiveRecord::Base.connecting_to(role: role)
     end
   end
-end
-
-if defined?(Rails::Railtie)
-  require "guard_rail/railtie"
-else
-  # just load everything immediately for Rails 2
-  GuardRail.initialize!
 end
--- readme.md
+++ readme.md
@@ -3,55 +3,27 @@ GuardRail
 
 ## About
 
-GuardRail allows multiple database environments and environment overrides to
-ActiveRecord, allowing least-privilege best practices.
+GuardRail is a thin wrapper around Rail 6.1's native role switching.
 
 ## Installation
 
-Add `gem 'guardrail'` to your Gemfile (tested with Rails 4.x and 5.x, and also
-with the release candidate for Rails 6.0)
+Add `gem 'guardrail'` to your Gemfile.
 
 ## Usage
 
-There are two major use cases for guardrail. The first is for primary/replica(/deploy) environments.
-Using a replica is as simple as adding a replica block (underneath your main environment block) in
-database.yml, then wrapping stuff you want to query the replica in GuardRail.activate(:replica) blocks.
-You can extend this to a deploy environment so that migrations will run as a deploy user that
-permission to modify schema, while your normal app runs with lower privileges that cannot modify
-schema. This is defense-in-depth in practice, so that *if* you happen to have a SQL injection
-bug, it would be impossible to do something like dropping tables.
+See https://guides.rubyonrails.org/active_record_multiple_databases.html. GuardRail simply adds
+syntactic sugar to easily switch to different roles:
 
-The other major use case is more defense-in-depth. By carefully setting up your environment, you
-can default to script/console sessions for regular users to use their own database user, and the
-replica.
-
-Example database.yml file:
-
-```yaml
-production:
-  adapter: postgresql
-  username: myapp
-  database: myapp
-  host: db-primary
-  replica:
-    host: db-replica
-  deploy:
-    username: deploy
-```
-
-Using an initializer, you can achieve the default environment settings (in tandem with profile
-changes):
 
 ```ruby
-if ENV['RAILS_DATABASE_ENVIRONMENT']
-  GuardRail.activate!(ENV['RAILS_DATABASE_ENVIRONMENT'].to_sym)
-end
-if ENV['RAILS_DATABASE_USER']
-  GuardRail.apply_config!(:username => ENV['RAILS_DATABASE_USER'])
+def some_method
+  GuardRails.activate(:secondary) do
+    MyModel.some_really_long_query
+  end
 end
 ```
 
-Additionally **in Ruby 2.0+** you can include GuardRail::HelperMethods and use several helpers
+Additionally you can include GuardRail::HelperMethods and use several helpers
 to execute methods on specific environments:
 
 ```ruby
